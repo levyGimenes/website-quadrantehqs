44a339a5f075ebe48cc1260ea2910c4170995d8d
Levy Gimenes <111082674+levyGimenes@users.noreply.github.com>
2023-06-18 13:35:21 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.comics-list', { duration: 1000 });
+ScrollReveal().reveal('.manga-list', { duration: 1000 });
+ScrollReveal().reveal('.news-list', { duration: 1000 });
+ScrollReveal().reveal('.post-list', { duration: 1000 });
+ScrollReveal().reveal('.comics', { duration: 1000 });
+ScrollReveal().reveal('.manga', { duration: 1000 });
+ScrollReveal().reveal('.news', { duration: 1000 });
+ScrollReveal().reveal('.post', { duration: 1000 });
